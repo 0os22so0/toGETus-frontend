3513f03cb3d4ad3824a9f95924f64a40c18f1d5f
minsol <ohimsoly@gmail.com>
2023-01-13 00:26:33 +0900

    UPDATE: tailwind.config.js 주석 삭제

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,23 +5,12 @@ module.exports = {
       backgroundImage: {
         "paper-texture": "url('images/paper-texture.png')",
       },
+      // colors: {},
     },
     screens: {
       sm: "375px", // 스맛폰
-      // => @media (min-width: 640px) { ... }
-
       md: "768px",
-      // => @media (min-width: 768px) { ... }
-
       lg: "100%",
-      // lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      // xl: "1280px",
-      // // => @media (min-width: 1280px) { ... }
-
-      // "2xl": "1536px",
-      // // => @media (min-width: 1536px) { ... }
     },
     container: {
       center: true,
