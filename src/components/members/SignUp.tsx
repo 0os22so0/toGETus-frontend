0b14f3fe46634a19f51c20ba5158f7d7d7bac311
minsol <ohimsoly@gmail.com>
2023-01-13 20:48:13 +0900

    프리티어 설정 추가 및 tailwind 애니메이션 시도

--- src/components/members/SignUp.tsx
+++ src/components/members/SignUp.tsx
@@ -1,5 +1,15 @@
 const SignUp = () => {
-  return <h1>Sign Up</h1>;
+  return (
+    <main className="flex flex-col justify-between items-center pt-[10vh]">
+      <h1>
+        <span className="animate-spin" style={{ transform: "rotate(14deg)" }}>
+          👋
+        </span>
+        <span className="text-togetus-emoji">어솽솽</span>
+        <span className="animate-waving-hand">👋</span>
+      </h1>
+    </main>
+  );
 };
 
 export default SignUp;
