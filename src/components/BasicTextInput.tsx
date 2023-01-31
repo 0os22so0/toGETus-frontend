912f70638e223530d95ec4d75c8fa66e6cb92ffa
minsol <ohimsoly@gmail.com>
2023-01-31 22:20:17 +0900

    formik: test

--- src/components/BasicTextInput.tsx
+++ src/components/BasicTextInput.tsx
@@ -1,7 +1,8 @@
 import { clsx } from 'clsx';
+import { Field } from 'formik';
 
 const BasicTextInput = (props: React.InputHTMLAttributes<HTMLInputElement>) => {
   const { className, disabled } = props;
-  return <input {...props} className={clsx('p-2 border rounded shadow', disabled && 'cursor-not-allowed', className)} />;
+  return <Field {...props} className={clsx('p-2 border rounded shadow', disabled && 'cursor-not-allowed', className)} />;
 };
 export default BasicTextInput;
--- src/components/members/Login.tsx
+++ src/components/members/Login.tsx
@@ -5,15 +5,21 @@ import BasicInput from 'components/BasicTextInput';
 import GoogleButton from 'components/members/GoogleButton';
 import { useNavigate } from 'react-router';
 import toGETusLogo from 'images/logo.png';
+import { Formik, Form, FormikHelpers } from 'formik';
+
+interface Values {
+  email: string;
+  password: string;
+}
 
 /**
- *
  * 로그인 페이지(/)
  * @returns
  */
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const initialValues: Values = { email: '', password: '' };
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
   const navigate = useNavigate();
 
@@ -27,27 +33,27 @@ const Login = () => {
     });
   };
 
-  const onLogin = async () => {
-    console.log({
-      userId: email,
-      password: password,
-    });
-
-    // await fetch("http://3.35.176.118:8080/api/login", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     userId: "test@test.com",
-    //     password: "1234",
-    //   }),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => alert(data));
-
-    navigate('/rest-room');
-  };
+  // const onLogin = async () => {
+  //   console.log({
+  //     userId: email,
+  //     password: password,
+  //   });
+  //
+  // await fetch("http://3.35.176.118:8080/api/login", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     userId: "test@test.com",
+  //     password: "1234",
+  //   }),
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => alert(data));
+  //
+  //   navigate('/rest-room');
+  // };
 
   return (
     <main className="flex flex-col justify-between items-center pt-[10vh]">
@@ -55,20 +61,33 @@ const Login = () => {
       <img src={toGETusLogo} alt={'togetus logo'} />
 
       {/* Login Form */}
-      <form className="m-0 flex flex-col w-[100%] gap-8 py-[10vh]">
-        <div className="flex flex-col gap-2">
-          <label>• 소셜 ID로 로그인</label>
-          <GoogleButton onSocial={onSocial} />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label>• 이메일 ID로 로그인</label>
-          <BasicInput type="email" placeholder="이메일 ID" onChange={(e) => setEmail(e.target.value)} />
-          <BasicInput type="password" placeholder="비밀번호" autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
-          <BasicButton className="bg-togetus-emoji" onClick={() => onLogin()}>
-            #가보자고!!
-          </BasicButton>
-        </div>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 500);
+        }}
+      >
+        <Form className="m-0 flex flex-col w-[100%] gap-8 py-[10vh]">
+          <div className="flex flex-col gap-2">
+            <label>• 소셜 ID로 로그인</label>
+            <GoogleButton onSocial={onSocial} />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label>• 이메일 ID로 로그인</label>
+            {/* <BasicInput type="email" placeholder="이메일 ID" onChange={(e) => setEmail(e.target.value)} />
+            <BasicInput type="password" placeholder="비밀번호" autoComplete="off" onChange={(e) => setPassword(e.target.value)} /> */}
+            <BasicInput id="email" type="email" placeholder="이메일 ID" />
+            <BasicInput id="password" type="new-password" placeholder="비밀번호" />
+            {/* <BasicButton className="bg-togetus-emoji" type='submit' onClick={() => onLogin()}> */}
+            <BasicButton className="bg-togetus-emoji" type="submit">
+              #가보자고!!
+            </BasicButton>
+          </div>
+        </Form>
+      </Formik>
       {/* Sign Up */}
       <Link to="/sign-up" className="underline text-stone-400">
         가입하기
