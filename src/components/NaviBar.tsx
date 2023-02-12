58bebadd15190fc562203f7030b85509203856fb
minsol <ohimsoly@gmail.com>
2023-02-12 16:48:30 +0900

    implement Navibar

--- src/components/NaviBar.tsx
+++ src/components/NaviBar.tsx
@@ -1,10 +1,14 @@
-// import { FaUserCircle } from "react-icons/fa";
-import { clsx } from 'clsx';
+import { Fragment } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import clsx from 'clsx';
+
 import ProfileImage from 'images/profile.jpg';
-import { useLocation } from 'react-router-dom';
+import toGETusLogo from 'images/logo.png';
 
 const NaviBar = () => {
   const { pathname } = useLocation();
+  const navigate = useNavigate();
 
   const checkShowNavi = () => {
     if (pathname === '/') return false;
@@ -12,21 +16,73 @@ const NaviBar = () => {
   };
 
   return checkShowNavi() ? (
-    <div className={clsx('flex justify-between align-middle p-3', 'bg-white border-b border-slate-200')}>
+    <div className="flex justify-between items-center p-3 bg-white border-b border-slate-200">
       {/* 왼쪽 */}
-      <div className="bg-red-200">
-        <span>left</span>
+      <div className="">
+        <span className="text-xs">Something</span>
       </div>
-      {/* 가운데 */}
-      <div className="bg-yellow-200">
-        <span>center</span>
-      </div>
-      {/* 오른쪽 */}
-      <button onClick={() => console.log('profile')}>
-        <div className="flex space-x-2">
-          <img src={ProfileImage} alt="profile" className="rounded-full w-[32px] h-[32px]" />
+
+      {/* Logo - toGETus */}
+      <img src={toGETusLogo} alt={'togetus logo'} className="h-[4vh]" />
+
+      {/* User Profile */}
+      {/* <button onClick={() => console.log('profile')}>
+        <img src={ProfileImage} alt="Profile Image" className="rounded-full w-[32px] h-[32px]" />
+      </button> */}
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button>
+            <img src={ProfileImage} alt="Profile Image" className={clsx('rounded-full w-[32px] h-[32px]')} />
+          </Menu.Button>
         </div>
-      </button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${active && 'bg-yellow-200'} flex w-full rounded-md px-2 py-2 text-sm`}
+                    onClick={() => {
+                      navigate('/rest-room/profile');
+                    }}
+                  >
+                    💡 내 정보
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${active && 'bg-yellow-200'} flex w-full rounded-md px-2 py-2 text-sm`}
+                    onClick={() => {
+                      navigate('/rest-room/profile/badge');
+                    }}
+                  >
+                    😎 내 와기뱃지들
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button className={`${active && 'bg-yellow-200'} flex w-full rounded-md px-2 py-2 text-sm`} onClick={() => navigate('/')}>
+                    👋 로그아웃
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </div>
   ) : (
     <></>
