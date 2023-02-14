80cf68336316055f26faad8550ff254b974b32bf
minsol <ohimsoly@gmail.com>
2023-02-14 21:48:40 +0900

    crest party #1

--- src/components/rest-room/CreateNewParty.tsx
+++ src/components/rest-room/CreateNewParty.tsx
@@ -1,8 +1,34 @@
+import BasicTextInput from 'components/BasicTextInput';
+
 const CreateNewParty = () => {
   return (
-    <>
-      <h1>3. New Party</h1>
-    </>
+    <form className="w-full flex flex-col justify-between items-center">
+      <InputWithLabel labelText="• 파티 이름" input={<BasicTextInput placeholder="toGETus" type="text" />} required={true} />
+
+      {/* 
+      1. 파티 이름
+      2. 파티 챌린지 기간
+      3. 인증 룰
+      */}
+    </form>
+  );
+};
+
+type PInputWithLabel = {
+  labelText: string;
+  input: JSX.Element;
+  required?: boolean;
+};
+const InputWithLabel = (props: PInputWithLabel) => {
+  const { labelText, input, required } = props;
+  return (
+    <div className="flex flex-col">
+      <label>
+        {labelText}
+        {required && <span className="text-red-600 text-sm"> *</span>}
+      </label>
+      {input}
+    </div>
   );
 };
 
