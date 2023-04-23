d2e8b517a97178dde76cb5133ab7a5925cca7cf7
minsol <ohimsoly@gmail.com>
2023-04-24 01:05:43 +0900

    파티 생성 #2

--- src/atoms/userProfile.ts
+++ src/atoms/userProfile.ts
@@ -1,4 +1,5 @@
 import { atom } from 'recoil';
+import { v1 } from 'uuid';
 
 export interface IUserProfileAtom {
   name: string;
@@ -6,10 +7,9 @@ export interface IUserProfileAtom {
 }
 
 export const userProfileState = atom<IUserProfileAtom>({
-  key: 'profile',
+  key: `userProfileState/${v1()}`,
   default: {
     name: '관악우파루파',
-    image:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.co.kr%2Fiamblackrose%2F%25EC%25A7%25B1%25EA%25B5%25AC%25EC%25A7%25A4%2F&psig=AOvVaw1YKxfAUTCT3ECzqbvrtBhD&ust=1682351047446000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKDZuZaswP4CFQAAAAAdAAAAABAE',
+    image: 'https://picsum.photos/200/200',
   },
 });
--- src/components/rest-room/CreateNewParty.tsx
+++ src/components/rest-room/CreateNewParty.tsx
@@ -12,8 +12,6 @@ type TRule = 'PM' | 'OTHER';
 const CreateNewParty = () => {
   const userProfile = useRecoilValue(userProfileState);
 
-  console.log(userProfile);
-
   const [dDays, setDDays] = useState<TDDays>(10);
   const [customDDay, setCustomDDay] = useState<number>(0);
   const [endDate, setEndDate] = useState<string>('');
@@ -34,114 +32,125 @@ const CreateNewParty = () => {
   }, [dDays, customDDay]);
 
   return (
-    <form className="w-full flex flex-col justify-between items-center gap-5">
-      {/* 파티 이름 */}
-      <InputWithLabel labelText="파티 이름" input={<BasicTextInput placeholder="toGETus" type="text" />} required={true} />
-      {/* 파티 챌린지 기간 */}
-      <InputWithLabel
-        labelText="파티 챌린지 기간"
-        input={
-          <>
+    <>
+      {/* 유저 프로필 */}
+      <div className="flex justify-center items-center gap-4 mb-10">
+        <img src={userProfile.image} alt="user profile image" className="w-[4rem] h-[4rem] rounded-full" />
+        <div className="text-lg">
+          <p>
+            <span className="text-xl">{userProfile.name}</span>님,
+          </p>
+          <p>새 일을 벌여봅시다!</p>
+        </div>
+      </div>
+      {/* 파티 폼 */}
+      <form className="w-full flex flex-col justify-between items-center gap-5">
+        {/* 파티 이름 */}
+        <InputWithLabel labelText="파티 이름" input={<BasicTextInput placeholder="toGETus" type="text" />} required={true} />
+        {/* 파티 챌린지 기간 */}
+        <InputWithLabel
+          labelText="파티 챌린지 기간"
+          input={
+            <>
+              <div className="w-full">
+                <label
+                  className={clsx(
+                    'inline-block w-1/3 h-[40px] rounded-l-full text-center p-2 border border-r-0 border-[#D9D9D9]',
+                    dDays === 10 ? 'bg-black text-white' : 'bg-white text-black',
+                    dDays === 'custom' && 'rounded-br-none'
+                  )}
+                >
+                  <input type="radio" name="d-day" value={10} onChange={() => setDDays(10)} defaultChecked className="hidden" /> 10일
+                </label>
+                <label
+                  className={clsx(
+                    'inline-block w-1/3 h-[40px] text-center p-2 border border-r-0',
+                    dDays === 100 ? 'bg-black text-white' : 'bg-white text-black'
+                  )}
+                >
+                  <input type="radio" name="d-day" value={100} onChange={() => setDDays(100)} className="hidden" /> 100일
+                </label>
+                <label
+                  className={clsx(
+                    'inline-block w-1/3 h-[40px] rounded-r-full text-center p-2 border',
+                    dDays === 'custom' ? 'bg-black text-white rounded-bl-none' : 'bg-white text-black'
+                  )}
+                >
+                  <input type="radio" name="d-day" value={'custom'} onChange={() => setDDays('custom')} className="hidden" /> 직접 입력
+                </label>
+                {dDays === 'custom' && (
+                  <BasicTextInput
+                    type="number"
+                    className="w-full mt-2"
+                    defaultValue={0}
+                    onChange={(e) => setCustomDDay(Number(e.target.value))}
+                  />
+                )}
+              </div>
+              <span className="mt-1 text-sm">예상 종료일: {endDate}</span>
+            </>
+          }
+          required={true}
+        />
+        {/* 파티 챌린지 목표 */}
+        <InputWithLabel
+          labelText="파티 챌린지 목표"
+          input={
+            <>
+              <BasicTextInput placeholder="파티 챌린지 목표" />
+              <BasicTextArea placeholder="필요 시 설명을 적어주세요." />
+            </>
+          }
+          required={true}
+        />
+        {/* 파티 인증 룰 */}
+        <InputWithLabel
+          labelText="인증 룰"
+          input={
             <div className="w-full">
               <label
                 className={clsx(
                   'inline-block w-1/3 h-[40px] rounded-l-full text-center p-2 border border-r-0 border-[#D9D9D9]',
-                  dDays === 10 ? 'bg-black text-white' : 'bg-white text-black',
-                  dDays === 'custom' && 'rounded-br-none'
+                  rule === 'PM' ? 'bg-black text-white' : 'bg-white text-black'
                 )}
               >
-                <input type="radio" name="d-day" value={10} onChange={() => setDDays(10)} defaultChecked className="hidden" /> 10일
-              </label>
-              <label
-                className={clsx(
-                  'inline-block w-1/3 h-[40px] text-center p-2 border border-r-0',
-                  dDays === 100 ? 'bg-black text-white' : 'bg-white text-black'
-                )}
-              >
-                <input type="radio" name="d-day" value={100} onChange={() => setDDays(100)} className="hidden" /> 100일
+                <input type="radio" name="auth-way" value="PM" onChange={() => setRule('PM')} defaultChecked className="hidden" /> 파티장만
               </label>
               <label
                 className={clsx(
                   'inline-block w-1/3 h-[40px] rounded-r-full text-center p-2 border',
-                  dDays === 'custom' ? 'bg-black text-white rounded-bl-none' : 'bg-white text-black'
+                  rule === 'OTHER' ? 'bg-black text-white' : 'bg-white text-black'
                 )}
               >
-                <input type="radio" name="d-day" value={'custom'} onChange={() => setDDays('custom')} className="hidden" /> 직접 입력
-              </label>
-              {dDays === 'custom' && (
+                <input type="radio" name="auth-way" value="OTHER" onChange={() => setRule('OTHER')} className="hidden" /> 구성원 비율
+              </label>{' '}
+              {rule === 'OTHER' && (
                 <BasicTextInput
                   type="number"
-                  className="w-full mt-2"
+                  className="w-full rounded-t-0"
                   defaultValue={0}
-                  onChange={(e) => setCustomDDay(Number(e.target.value))}
+                  onChange={(e) => setCustomRule(Number(e.target.value))}
                 />
               )}
             </div>
-            <span className="mt-1 text-sm">예상 종료일: {endDate}</span>
-          </>
-        }
-        required={true}
-      />
-      {/* 파티 챌린지 목표 */}
-      <InputWithLabel
-        labelText="파티 챌린지 목표"
-        input={
-          <>
-            <BasicTextInput placeholder="파티 챌린지 목표" />
-            <BasicTextArea placeholder="필요 시 설명을 적어주세요." />
-          </>
-        }
-        required={true}
-      />
-
-      {/* 파티 인증 룰 */}
-      <InputWithLabel
-        labelText="인증 룰"
-        input={
-          <div className="w-full">
-            <label
-              className={clsx(
-                'inline-block w-1/3 h-[40px] rounded-l-full text-center p-2 border border-r-0 border-[#D9D9D9]',
-                rule === 'PM' ? 'bg-black text-white' : 'bg-white text-black'
-              )}
-            >
-              <input type="radio" name="auth-way" value="PM" onChange={() => setRule('PM')} defaultChecked className="hidden" /> 파티장만
-            </label>
-            <label
-              className={clsx(
-                'inline-block w-1/3 h-[40px] rounded-r-full text-center p-2 border',
-                rule === 'OTHER' ? 'bg-black text-white' : 'bg-white text-black'
-              )}
-            >
-              <input type="radio" name="auth-way" value="OTHER" onChange={() => setRule('OTHER')} className="hidden" /> 구성원 비율
-            </label>{' '}
-            {rule === 'OTHER' && (
-              <BasicTextInput
-                type="number"
-                className="w-full rounded-t-0"
-                defaultValue={0}
-                onChange={(e) => setCustomRule(Number(e.target.value))}
-              />
-            )}
-          </div>
-        }
-        required={true}
-      />
-      {/* 파티 입장 퀴즈 */}
-      <InputWithLabel
-        labelText="파티 입장 퀴즈"
-        input={
-          <>
-            <BasicTextInput placeholder="질문을 입력해주세요." />
-            <BasicTextInput placeholder="답변을 입력해주세요." />
-          </>
-        }
-      />
-
-      <BasicButton className="bg-black text-white" onClick={() => console.log('create party')}>
-        한 판 벌여보세
-      </BasicButton>
-    </form>
+          }
+          required={true}
+        />
+        {/* 파티 입장 퀴즈 */}
+        <InputWithLabel
+          labelText="파티 입장 퀴즈"
+          input={
+            <>
+              <BasicTextInput placeholder="질문을 입력해주세요." />
+              <BasicTextInput placeholder="답변을 입력해주세요." />
+            </>
+          }
+        />
+        <BasicButton className="bg-black text-white" onClick={() => console.log('create party')}>
+          한 판 벌여보세
+        </BasicButton>
+      </form>
+    </>
   );
 };
 
